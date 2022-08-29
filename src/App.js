8ff648536d04a0bb37fe8e25190501c36022a7a2
Gustavo <gustgouvea@hotmail.com>
2022-08-29 11:34:37 -0300

    categorias e row sendo desenvolvidos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import Row from "./components/Row";
+import categories from './api';
 
 function App() {
-  return <div className="App">
-  {/*Navbar*/}
-  {/*Destaque*/}
-  {/*Em alta*/}
-  {/*Filmes de cada categoria*/}
-  </div>;
+  return (
+    <div className="App">
+      {/*Navbar*/}
+      {/*Destaque*/}
+      {/*Em alta*/}
+      {categories.map(category => {
+        return <Row />;
+      })}      
+    </div>
+  ); 
 }
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -4,7 +4,44 @@ const categories = [
     {
         name: "trending",
         title: "Em alta",
-        path: ""
+        path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`
+    },
+    {
+        name: "netflixOriginals",
+        title: "Originais Netflix",
+        path: `/discover/tv?api_key=${API_KEY}&with_networks=213`
+    },
+    {
+        name: "topRated",
+        title: "Populares",
+        path: `/movie/top_rated?api_key=${API_KEY}&language=pt-BR`
+    },
+    {
+        name: "comedy",
+        title: "Comédias",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=35`
+    },
+    {
+        name: "romances",
+        title: "Romances",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=1074`
+    },
+    {
+        name: "documentaries",
+        title: "Documentários",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=99`
+    },
+];
+
+export const getMovies = async (path) => {
+    try {
+      let url = `https://api.themoviedb.org/3/${path}`;
+      const response = await fetch(url);
+      return await response.json()
+    } catch (error) {
+      console.log("error getMovies: ", error);
     }
-]
+};
+
+export default categories;
 
